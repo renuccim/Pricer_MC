28885722b82eb925a0ced108f4098c5d0595196a
Najib Kachmat <Najib.Kachmat@ensimag.grenoble-inp.fr>
2014-09-19 11:59:57 +0200

    prix t=0

---
+++ Equipe_5/CMakeFiles/parser-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/parser-test.dir/src/parser.cpp.o"
+  "CMakeFiles/parser-test.dir/src/test_parser.cpp.o"
+  "parser-test.pdb"
+  "parser-test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/parser-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Equipe_5/CMakeFiles/pricer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pricer.dir/src/basket.cpp.o"
+  "CMakeFiles/pricer.dir/src/barrier_l.cpp.o"
+  "CMakeFiles/pricer.dir/src/barrier_u.cpp.o"
+  "CMakeFiles/pricer.dir/src/asian.cpp.o"
+  "CMakeFiles/pricer.dir/src/barrier.cpp.o"
+  "CMakeFiles/pricer.dir/src/performance.cpp.o"
+  "CMakeFiles/pricer.dir/src/bs.cpp.o"
+  "CMakeFiles/pricer.dir/src/mc.cpp.o"
+  "CMakeFiles/pricer.dir/src/parser.cpp.o"
+  "CMakeFiles/pricer.dir/src/pricer.cpp.o"
+  "pricer.pdb"
+  "pricer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pricer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
